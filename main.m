cbc3f50e36ead85de205461d17b365735e383314
Niloofar Assari <assariniloofar88@gmail.com>
2017-02-05 12:58:06 -0800

    compact binary face descriptor

---
+++ main.m
@@ -0,0 +1,38 @@
+%% CBFD Implementation
+close all;clear;clc;
+
+addpath vendors
+
+% Set parameters
+params.ht = 128;
+params.wt = 128;
+params.N = 8;
+params.M = 8; 
+params.lambda1 = 0.001;
+params.lambda2 = 0.0001;
+params.binsize = 15;
+params.dictsize = 500;
+params.n_iter = 20;
+params.coeff = 1000;
+
+%{ TRAIN }%
+cbfd_train;
+%{ TRAIN }%
+
+load datasets\feret\gallery.mat    
+ytr = label;
+Xtrain = extractFeature(data,W,D,params);
+[eigvec2,eigval,~,sampleMean] = PCA(Xtrain);
+eigvec = (bsxfun(@rdivide,eigvec2',sqrt(eigval))');
+xtr = (bsxfun(@minus, Xtrain, mean(Xtrain))*eigvec(:,1:params.coeff))';
+
+probe{1}='dup1'; probe{2}='dup2';
+for i =1:2
+    load(['feret\' probe{i} '.mat']);
+    yts = label;
+    Xtest = extractFeature(data,W,D,params);
+    xts = (bsxfun(@minus, Xtest, sampleMean)*eigvec(:,1:params.coeff))';
+    acc(i) = NN(xtr',ytr',xts',yts',2);
+end
+fprintf('\nRank-1 recognition rate for %s set:%2.2f\n',probe{1},acc(1));
+fprintf('\nRank-1 recognition rate for %s set:%2.2f\n',probe{2},acc(2));
